2f3b7c7fee9a6cbdaf720660acba91780bf68b79
Ken Domino <kenneth.domino@domemtech.com>
2018-01-02 16:30:43 -0500

    Hoping--maybe--that this is finally how to get nuget/msbuild working with native dlls and across different targets--due to completely horrible documentation from MS or Nuget folks.

--- .nuget/pack.bat
+++ .nuget/pack.bat
@@ -1,2 +1,2 @@
 del swigged.cuda.9.1.85.1.nupkg
-nuget pack swigged.cuda.nuspec -Version 9.1.85.1 -Properties 'Configuration=Release;Platform=x86;'
+nuget pack swigged.cuda.nuspec -Version 9.1.85.2 -Properties 'Configuration=Release;Platform=x86;'
