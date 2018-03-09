0c9b91806cca6eafc1d71066b628042d82de2800
Ken Domino <kenneth.domino@domemtech.com>
2018-03-09 14:56:42 -0500

    Update for release.

--- .nuget/pack.bat
+++
@@ -1,2 +0,0 @@
-del swigged.cuda.9.1.85.1.nupkg
-nuget pack swigged.cuda.nuspec -Version 9.1.85.2 -Properties 'Configuration=Release;Platform=x86;'
---
+++ nuget/pack.bat
@@ -0,0 +1,1 @@
+nuget pack swigged.cuda.nuspec -Version 9.185.3 -Properties 'Configuration=Release;Platform=x86;'
