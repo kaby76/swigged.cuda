c13d3112210dd17629fca97f4130a4a2883163d3
Ken Domino <kenneth.domino@domemtech.com>
2018-04-29 20:10:55 -0400

    Adding a few items to help set up and build CUDA files.

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 nvcc --compiler-options '-fpic' -c CUDA_wrap.cpp
 g++ -o "libnew-swigged-cuda-native.so.1.0" -Wl,--verbose -Wl,--no-undefined -Wl,-L"/usr/local/cuda-9.1/lib64" -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack -shared "CUDA_wrap.o" -lcuda
+
---
+++ kens-nvcc.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+export PATH=/usr/local/cuda-9.1/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+echo nvcc --compiler-options '-fpic' -c
+
+nvccargs=""
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    echo $key
+    shift
+
+    case $key in
+        -c) # specifies compile only
+            nvccargs="$nvccargs -c "
+            ;;
+        -x) # specifies the language
+#           nvccargs="$nvccargs --compiler-options '-x $1' "
+            shift
+            ;;
+        -I)
+            nvccargs="$nvccargs -I'$1' "
+            shift
+            ;;
+        -W*)
+#           nvccargs="$nvccargs --compiler-options '$key' "
+            ;;
+        -o)
+            file=$1
+            nvccargs="$nvccargs -o $1 "
+            shift
+            ;;
+        -g1)
+#           nvccargs="$nvccargs --compiler-options -g1 "
+            ;;
+        -O3)
+#           nvccargs="$nvccargs --compiler-options -O3 "
+            ;;
+        -D*)
+            nvccargs="$nvccargs $key "
+            ;;
+        -f*)
+            nvccargs="$nvccargs --compiler-options $key "
+            ;;
+        -std*)
+#           nvccargs="$nvccargs --compiler-options $key "
+            ;;
+        *)
+            nvccargs=" $key "
+            ;;
+
+    esac
+
+done
+
+echo nvcc --verbose -c $nvccargs
+nvcc --verbose -c $nvccargs
